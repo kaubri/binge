9001a3294df485eb19e29eeb9e53cb02011968aa
vanJoseph <donovan.wilder@gmail.com>
2017-04-09 18:49:23 -0400

    donovan initial test

---
+++ app/src/main/java/com/example/android/binge/Episode.java
@@ -0,0 +1,12 @@
+package com.example.android.binge;
+
+/**
+ * A model for the data of an episode.
+ *
+ * @author Donovan J. Wilder
+ * @since 2017-04-09
+ * @version 1.0
+ */
+
+public class Episode {
+}
